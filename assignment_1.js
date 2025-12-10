1822f610ebe342457d71dfbacf2e8244958544a9
[lucadm] <[lucadimaio@gmail.com]>
2025-12-10 19:06:01 +0000

    Add end of chapter challenge

---
+++ assignment_1.js
@@ -0,0 +1,25 @@
+/* 
+returns an array of lotto numbers
+numbers should be 1=<x=<47 and be random
+does not accept 39 or 17
+no duplication of numbers
+skip 2 unlucky numbers
+*/
+
+function getLottoNums() {
+  const lottoDrawList = [];
+  while (lottoDrawList.length < 5) {
+    const lottoNum = Math.floor(Math.random() * 47 + 1);
+    console.log("current num", lottoNum);
+    if (lottoNum === 39 || lottoNum === 17) {
+      continue;
+    }
+    if (!lottoDrawList.includes(lottoNum)) {
+      lottoDrawList.push(lottoNum);
+    }
+  }
+  console.log("final list", lottoDrawList);
+  return lottoDrawList;
+}
+
+getLottoNums();
